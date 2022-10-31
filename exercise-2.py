20ac534f224982fa6bff6ce0d0d250c647dbcfe7
julianboyne <julianboyne11@gmail.com>
2022-10-31 17:56:45 -0400

    implement work/phrase lenght functionality

--- exercise-2.py
+++ exercise-2.py
@@ -2,8 +2,14 @@
 
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
+phrase = input("Please enter a word or phrase: ")
 #      Please enter a word or phrase: 
 # 2. Print the following message:
+
+
+while phrase != "quit":
+  phrase = input("Please enter a word or phrase: ")
+  print(len(phrase))
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
