efdef4ba997f6ca77d3aa7165088388b50973b91
julianboyne <julianboyne11@gmail.com>
2022-10-31 18:27:13 -0400

    implement triangle type functionality

--- exercise-4.py
+++ exercise-4.py
@@ -2,10 +2,20 @@
 
 # Write the code that:
 # 1. Prompts the user to enter the three lengths of a triangle (one at a time):
+print('Enter the lengths of three side of a triangle:')
+a = input('a: ')
+b = input('b: ')
+c = input('c: ')
 #      Enter the lengths of three side of a triangle:
 #      a: 
 #      b:
 #      c:
+if a == b and b == c:
+  print(f'A triangle with sides of {a}, {b} & {c} is an equalateral triangle')
+elif a != b and b != c:
+  print(f'A triangle with sides of {a}, {b} & {c} is an scalene triangle')
+else:
+  print(f'A triangle with sides of {a}, {b} & {c} is an isosceles triangle')
 # 2. Write the code that determines if the triangle is:
 #      equalateral - all three sides are equal in length
 #      scalene - all three sides are unequal in length
